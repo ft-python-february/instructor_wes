c297ef03883506bec3d761d818c314eed0d6375b
Weston Harper <wes@tao.team>
2019-02-05 16:49:17 -0700

    added day 2

---
+++ python/oop/animal.py
@@ -0,0 +1,34 @@
+class Animal:
+  def __init__(self, name):
+    self.health = 100
+    self.alive = True
+    self.name = name
+
+  def display_health(self):
+    print(self.health)
+  
+  def introduce(self):
+    print("Hello, my name is {}".format(self.name))
+
+class Human(Animal):
+  def __init__(self, name, eye_color):
+    super().__init__(name)
+    self.eye_color = eye_color
+
+  def workout(self):
+    self.health += 10
+    return self
+
+  def introduce(self):
+    print("Hello, I am a human and my name is {}".format(self.name))
+
+
+todd = Human("Todd", "Purple")
+wes = Human("Wes", "Purple")
+jenny = Human("Jenny", "Purple")
+sam = Human("Sam", "Purple")
+david = Human("David", "Purple")
+
+david.workout().display_health()
+
+jenny.introduce()
---
+++ python/oop/demo.js
@@ -0,0 +1,6 @@
+var arr = [1, 2, 3, 4];
+
+arr.push(5);
+arr.pop();
+
+arr.length;
