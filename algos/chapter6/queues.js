f43c4799d086966803b0ca1ac906571dcfdfd102
Weston Harper <wes@tao.team>
2019-02-28 09:13:38 -0700

    finished queue

--- algos/chapter6/queues.js
+++ algos/chapter6/queues.js
@@ -29,8 +29,89 @@ class SLQueue{
 
     return this;
   }
+
+  // SLQueue: Front
+  // Create SLQueue method front() to return the value at front of our queue, without removing it.
+  front(){
+    if(!this.head){
+      return null;
+    }
+    return this.head.val;
+  }
+
+  // SLQueue: Is Empty
+  // Create SLQueue method isEmpty() that returns whether our queue contains no values.
+  isEmpty(){
+    // if(!this.head){
+    //   return true;
+    // }
+    // return false;
+    // return !this.head ? true : false;
+    return !this.head;
+  }
+
+  // SLQueue: Compare Queues
+  // Given two SLQueue objects, create a standalone function that returns whether they are equal.Queues are equal only if they have equal elements in identical order.Allocate no other object, and return the queues in their original condition upon exit.
+  compare(otherqueue){
+    if (this.length !== otherqueue.length){
+      return false;
+    }
+    let curr = this.head;
+    let currother = otherqueue.head;
+    while(curr) {
+      if (curr.val !== currother.val){
+        return false;
+      }
+      curr = curr.next;
+      currother = currother.next;
+    }
+    return true;
+  }
+  // SLQueue: Dequeue
+  // Create SLQueue method dequeue() to remove and return value at front of queue.Remember, SLQueue uses singly linked list(not array).
+  dequeue(){
+    if(!this.head){
+      throw new Error("Cannot dequeue, queue is empty.");
+    }
+    const temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length --;
+    return temp.val;
+  }
+
+  // SLQueue: Contains
+  // Create method contains(val) to return whether given value is found within our queue.
+  contains(val){
+    let curr = this.head;
+    while(curr){
+      if (curr.val == val){
+        return true;
+      }      
+      curr = curr.next;
+    }
+    return false;
+  }
+
+
+  size() {
+    return this.length;
+  }
+
 }
 
 const queue = new SLQueue();
+// console.log(queue.isEmpty());
+// console.log(queue.front());
 console.log(queue.enqueue("A").enqueue("B").enqueue("C"))
-console.log(queue.enqueue("D"))
+console.log(queue.contains("A"));
+console.log(queue.contains("X"));
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+// console.log(queue.enqueue("D"))
+// console.log(queue.isEmpty());
+// const other = new SLQueue
+// console.log(other.enqueue("A").enqueue("B").enqueue("C").enqueue('D'))
+// console.log(queue.compare(other))
